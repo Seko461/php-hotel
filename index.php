3d8e3d401c5ee6967eeaa44b86d7ea6cde3c63a6
Seko461 <sekopierre@gmail.com>
2024-04-20 08:06:21 +0200

    initial commit and table print on document

---
+++ index.php
@@ -0,0 +1,96 @@
+<?php
+
+ $hotels = [
+
+        [
+            'name' => 'Hotel Belvedere',
+            'description' => 'Hotel Belvedere Descrizione',
+            'parking' => true,
+            'vote' => 4,
+            'distance_to_center' => 10.4
+        ],
+        [
+            'name' => 'Hotel Futuro',
+            'description' => 'Hotel Futuro Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 2
+        ],
+        [
+            'name' => 'Hotel Rivamare',
+            'description' => 'Hotel Rivamare Descrizione',
+            'parking' => false,
+            'vote' => 1,
+            'distance_to_center' => 1
+        ],
+        [
+            'name' => 'Hotel Bellavista',
+            'description' => 'Hotel Bellavista Descrizione',
+            'parking' => false,
+            'vote' => 5,
+            'distance_to_center' => 5.5
+        ],
+        [
+            'name' => 'Hotel Milano',
+            'description' => 'Hotel Milano Descrizione',
+            'parking' => true,
+            'vote' => 2,
+            'distance_to_center' => 50
+        ],
+
+    ];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Hotels</title>
+</head>
+<body>
+
+<main>
+<section>
+<div class="container">
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Description</th>
+      <th scope="col">Parking</th>
+      <th scope="col">Vote</th>
+    </tr>
+  </thead>
+  <tbody>
+
+  <?php foreach($hotels as $index => $hotel) : ?>
+  
+  
+  
+    <tr>
+        <th scope="row"><?= $index+1 ?></th>
+        <td><?= $hotel['name'] ?></td>
+        <td><?= $hotel['description'] ?></td>
+        <td><?= $hotel['parking'] ?></td>
+        <td><?= $hotel['vote'] ?></td>
+    </tr>
+    
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
+</div>
+
+
+</section>
+
+
+</main>
+
+</body>
+</html>
