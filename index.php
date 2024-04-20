d4d63742af004a91050790c944b37afad88d4902
Seko461 <sekopierre@gmail.com>
2024-04-20 08:31:22 +0200

    bonus

--- index.php
+++ index.php
@@ -40,6 +40,13 @@
 
     ];
 
+    //var_dump(isset($_GET['parking']));
+
+    if (isset($_GET['parking'])) {
+        $hotels = array_filter($hotels, fn($hotel) => $hotel['parking']);
+    }
+
+
 ?>
 
 <!DOCTYPE html>
@@ -49,11 +56,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/f54afad4a3.js" crossorigin="anonymous"></script>
     <title>Hotels</title>
 </head>
 <body>
 
 <main>
+    <div class="container" >
+        <div class="row my-3 g-3">
+<form action="" method="get">
+<div class="form-check">
+    <input class="form-check-input" type="checkbox" value="yes" name="parking" id="parking">
+    <label class="form-check-label" for="parking">Parking</label>
+</div>
+<button class="btn btn-primary" type="submit">Search</button>
+<button class="btn btn-secondary" type="reset">Reset</button>
+
+</form>
+</div>
+</div>
+
+
 <section>
 <div class="container">
 <table class="table">
@@ -76,7 +99,7 @@
         <th scope="row"><?= $index+1 ?></th>
         <td><?= $hotel['name'] ?></td>
         <td><?= $hotel['description'] ?></td>
-        <td><?= $hotel['parking'] ?></td>
+        <td><?= $hotel['parking'] ? '<i class="fa-solid fa-square-parking"></i>' : '<i class="fa-solid fa-x"></i>'?> </td>
         <td><?= $hotel['vote'] ?></td>
     </tr>
     
